21c9443cc62250e4a90425bf78b4d941bee02d7d
H2ck22 <h2ck@yandex.ru>
2022-12-02 21:27:58 +0300

    Added lesson 10.1 StringTheory

---
+++ src/com/dmdev/lessons/lesson10/part2/Practice.java
@@ -0,0 +1,5 @@
+package com.dmdev.lessons.lesson10.part2;
+
+public class Practice {
+
+}
---
+++ src/com/dmdev/lessons/lesson7/practice/sundayPractice/task1/BoxUtil.java
@@ -0,0 +1,7 @@
+package com.dmdev.lessons.lesson7.practice.sundayPractice.task1;
+
+public class BoxUtil {
+    public static double volumeCalculate(double length, double width, double height) {
+        return length * width * height;
+    }
+}
