a6083a9aedf97f69022269bc6c283e25c8a78bef
H2ck22 <h2ck@yandex.ru>
2023-01-12 01:20:06 +0300

    add practice 15.1

---
+++ src/com/dmdev/lessons/lesson15/Ork.java
@@ -0,0 +1,20 @@
+package com.dmdev.lessons.lesson15;
+
+import java.util.Random;
+
+public class Ork extends Enemy {
+    public Ork(String name, int health, int damage) {
+        super(name, health, damage);
+    }
+
+    @Override
+    void attackHero(Hero hero) {
+        System.out.println(getName() + " ударил кинжалом " + hero.getName());
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            System.out.println(getName() + " использовал отравленный клинок (урон +5!)");
+            setDamage(getDamage() + 5);
+        }
+        super.attackHero(hero);
+    }
+}
---
+++ src/com/dmdev/lessons/lesson15/TrainingGround.java
@@ -0,0 +1,19 @@
+package com.dmdev.lessons.lesson15;
+
+public class TrainingGround {
+    public static void main(String[] args) {
+        Hero warrior = new Warrior("Боромир", 50, 15);
+        Hero mage = new Mage("Гендальф",50, 20);
+        Hero archer = new Archer("Леголас", 50, 10);
+        Enemy enemy = new Zombie("Зомби", 50, 5);
+        heroAttack(enemy, warrior, mage, archer);
+    }
+
+    public static void heroAttack(Enemy enemy, Hero... heroes) {
+        for (Hero hero : heroes) {
+            while (enemy.isAlive()) {
+                hero.attackEnemy(enemy);
+            }
+        }
+    }
+}
